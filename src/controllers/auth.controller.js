ba6ffdc7b4a32ae10b64785cf289a966a37b93d0
Angel Martinez <angel.codes139@gmail.com>
2020-10-20 13:50:22 -0500

    ✨ feature(auth): generate jsonwebtoken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const argon2 = require('argon2');
+const jtw = require('jsonwebtoken');
 
 // models
 const User = require('../models/user.model');
@@ -7,8 +8,8 @@ exports.authenticate = async (req, res) => {
   const { email, password } = req.body;
 
   // search the user
-  const userTryingAuth = await User.findOne({ email });
-  if (!userTryingAuth) {
+  const user = await User.findOne({ email });
+  if (!user) {
     // send error
     return res.status(401).json({
       response: 'fail',
@@ -17,8 +18,23 @@ exports.authenticate = async (req, res) => {
   }
 
   // check if the password match
-  if (await argon2.verify(userTryingAuth.password, password)) {
-    console.log('same password');
+  if (await argon2.verify(user.password, password)) {
+    // create a jsonwebtoken
+    const token = jtw.sign(
+      {
+        id: user.id,
+        name: user.name
+      },
+      process.env.SECRET,
+      {
+        expiresIn: '8h'
+      }
+    );
+    // return the token
+    res.status(200).json({
+      response: 'success',
+      data: token
+    });
   } else {
     // send error
     return res.status(401).json({
